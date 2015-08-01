240cce76e6ea433d085ab8529bbbe6420691ed87
MacBKPro <macbkpro@MacBKPros-MBP.hsd1.ca.comcast.net>
2015-08-01 13:27:07 -0700

    basic functions added

--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        println("typeing")
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * 0.2
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+    }
+    
+    
 
 
 }
